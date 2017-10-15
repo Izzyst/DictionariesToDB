ad93d70ac57f3bfea3b10fa13d6c51d047fe5c8a
Izzyst <desert.gaara00@gmail.com>
2017-10-15 12:49:20 +0200

    insert to database from csvFile

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -17,26 +17,7 @@ namespace ConsoleApp1
             {
                 using (var transaction = session.BeginTransaction())
                 {
-                    // create a couple of Stores each with some Products and Employees
-                    var barginBasin = new Word {W = "Table"};
-                    var superMart = new Word {W = "Bar"};
-
-                    var potatoes = new Definition {Def = "stol"};
-                    var fish = new Definition {Def = "bar"};
-                    var milk = new Definition {Def = "belka"};
-                    var bread = new Definition {Def = "listwa"};
-                    var cheese = new Definition {Def = "baton"};
-
-
-                    // add products to the stores, there's some crossover in the products in each
-                    // store, because the store-product relationship is many-to-many
-                    AddDefToWord(barginBasin, potatoes, fish, milk, bread, cheese);
-                    AddDefToWord(superMart, bread, cheese);
-
-                    // save both stores, this saves everything else via cascading
-                    session.SaveOrUpdate(barginBasin);
-                    session.SaveOrUpdate(superMart);
-                    /*FromExcelFileFactory ob = new FromExcelFileFactory();
+                    FromExcelFileFactory ob = new FromExcelFileFactory();
                     var list = ob.GetWords();
 
                     foreach (var word in list)
@@ -48,41 +29,17 @@ namespace ConsoleApp1
                             var d = new Definition {Def = def};
                             w.AddDefinition(d);
                         }
-                    }*/
+                        session.SaveOrUpdate(w);
+                    }
 
                     transaction.Commit();
                 }
 
-                // retreive all stores and display them
-                using (session.BeginTransaction())
-                {
-                    var stores = session.CreateCriteria(typeof(Word))
-                        .List<Word>();
-
-                    foreach (var store in stores)
-                    {
-                        WriteStorePretty(store);
-                    }
-                }
-
-                Console.ReadKey();
             }
             
             Console.ReadKey();
         }
 
-        private static void WriteStorePretty(object store)
-        {
-            Console.WriteLine("{0}", store.ToString());
-        }
-
-        public static void AddDefToWord(Word word, params Definition[] defs)
-        {
-            foreach (var def in defs)
-            {
-                word.AddDefinition(def);
-            }
-        }
 
 
     }
